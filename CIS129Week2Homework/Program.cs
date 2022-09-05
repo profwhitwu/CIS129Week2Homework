b8d34930da4396f547998809ed700a2657c4b124
Whit Wu <whit.wu@gmail.com>
2022-09-04 20:18:30 -0400

    Add project files.

---
+++ CIS129Week2Homework/Program.cs
@@ -0,0 +1,32 @@
+﻿//  1. Use explicit typing to define a variable to represent a first name.  Use that variable to store your first name.
+
+//  2. Using the variable defined above, print out its value using Console.WriteLine.
+
+//  3. Use implicit typing to define a variable to represent your last name.Use that variable to store your last name.
+
+//  4. Create an enum type that represents different kinds of movie genres (i.e. Action, Adventure, Romance).
+//  Then define a variable that stores one of these genres.
+
+//  5. Create a structure of a person, named Person.  The structure should have the following properties:
+//  FirstName, LastName, Age.  You do not need to create an instance of this record.
+
+//  6. Say we have a varaible whose value can only be true or false, but also could contain null.
+//  Define that variable here and store a value of true in it.
+
+//  7. Create a variable that holds that holds the monetary value of $5.99.
+
+//  8. Using explicit typing, create a variable to hold the value of 2147483648.
+//  The variable must be an integral numeric type. 
+
+//  9. Create a tuple that contains one string, and one int value.
+
+//  10. Using the tuple you created above, use Console.WriteLine to
+//  print the value of the first item of the tuple to the console.
+
+//  11. Copy your structure from question 5, and call it AnotherPerson.
+//  Replace the struct keyword with a reference type that will allow us to measure equality BY VALUE.
+
+//  12. Make another copy of your structure from question 5, and call it YetAnotherPerson.
+//  Replace the struct keyword with a reference type that will allow us to measure equality BY ADDRESS.
+
+
