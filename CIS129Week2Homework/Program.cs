99d792d451ce42e5f80905334041fb4bc3179e0c
profwhitwu <111308489+profwhitwu@users.noreply.github.com>
2022-09-09 18:10:31 -0400

    Update Program.cs

--- CIS129Week2Homework/Program.cs
+++ CIS129Week2Homework/Program.cs
@@ -4,12 +4,6 @@
 
 //  3. Use implicit typing to define a variable to represent your last name.Use that variable to store your last name.
 
-//  4. Create an enum type that represents different kinds of movie genres (i.e. Action, Adventure, Romance).
-//  Then define a variable that stores one of these genres.
-
-//  5. Create a structure of a person, named Person.  The structure should have the following properties:
-//  FirstName, LastName, Age.  You do not need to create an instance of this record.
-
 //  6. Say we have a varaible whose value can only be true or false, but also could contain null.
 //  Define that variable here and store a value of true in it.
 
@@ -22,6 +16,12 @@
 
 //  10. Using the tuple you created above, use Console.WriteLine to
 //  print the value of the first item of the tuple to the console.
+
+//  4. Create an enum type that represents different kinds of movie genres (i.e. Action, Adventure, Romance).
+//  Then define a variable that stores one of these genres.
+
+//  5. Create a structure of a person, named Person.  The structure should have the following properties:
+//  FirstName, LastName, Age.  You do not need to create an instance of this record.
 
 //  11. Copy your structure from question 5, and call it AnotherPerson.
 //  Replace the struct keyword with a reference type that will allow us to measure equality BY VALUE.
